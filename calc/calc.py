507b2d8659b21aa84546bcc9ee52169433479308
jayjaychukwu <liljaydon.pj@gmail.com>
2023-05-09 23:03:16 +0100

    worked on test cases for average method and solved failed tests

--- calc/calc.py
+++ calc/calc.py
@@ -22,3 +22,17 @@ class Calc:
             return a / b
         except ZeroDivisionError as err:
             return "inf"
+
+    def avg(self, it, lt=None, ut=None):
+        _it = it[:]
+
+        if lt is not None:
+            _it = [x for x in _it if x >= lt]
+
+        if ut is not None:
+            _it = [x for x in _it if x <= ut]
+
+        if not len(_it):
+            return 0
+
+        return sum(_it) / len(_it)
--- tests/test_calc.py
+++ tests/test_calc.py
@@ -71,3 +71,83 @@ def test_mul_by_zero_raises_exception():
 
     with pytest.raises(ValueError):
         c.mul(3, 0)
+
+
+def test_avg_correct_average():
+    c = Calc()
+
+    res = c.avg([2, 5, 12, 98])
+
+    assert res == 29.25
+
+
+def test_avg_removes_upper_outliers():
+    c = Calc()
+
+    res = c.avg([2, 5, 12, 98], ut=90)
+
+    assert res == pytest.approx(6.333333)
+
+
+def test_avg_removes_lower_outliers():
+    c = Calc()
+
+    res = c.avg([2, 5, 12, 98], lt=10)
+
+    assert res == pytest.approx(55)
+
+
+def test_avg_upper_threshold_is_included():
+    c = Calc()
+
+    res = c.avg([2, 5, 12, 98], ut=98)
+
+    assert res == 29.25
+
+
+def test_avg_lower_threshold_is_included():
+    c = Calc()
+
+    res = c.avg([2, 5, 12, 98], lt=2)
+
+    assert res == 29.25
+
+
+def test_avg_empty_list():
+    c = Calc()
+
+    res = c.avg([])
+
+    assert res == 0
+
+
+def test_avg_manages_empty_list_after_outlier_removal():
+    c = Calc()
+
+    res = c.avg([12, 98], lt=15, ut=90)
+
+    assert res == 0
+
+
+def test_avg_manages_empty_list_before_outlier_removal():
+    c = Calc()
+
+    res = c.avg([], lt=15, ut=90)
+
+    assert res == 0
+
+
+def test_avg_manages_zero_value_lower_outlier():
+    c = Calc()
+
+    res = c.avg([-1, 0, 1], lt=0)
+
+    assert res == 0.5
+
+
+def test_avg_manages_zero_value_upper_outlier():
+    c = Calc()
+
+    res = c.avg([-1, 0, 1], ut=0)
+
+    assert res == -0.5
